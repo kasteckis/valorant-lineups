da8f6b799e16208d3662d903e08eba790cd4d670
Valentinas Kasteckis <valentinas.kasteckis@nfq.lt>
2022-09-21 16:31:56 +0300

    Abilit to switch between light/dark modes easily

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import {Box} from "@mui/material";
 import AgentSelection from "../components/AgentSelection";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'light',
+    },
+});
 
 const Home: NextPage = () => {
     return (
@@ -13,26 +21,31 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Box sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                p: 1,
-                m: 1,
-                borderRadius: 1,
-            }}>
-                <AgentSelection />
-            </Box>
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline />
 
-            <a
-                style={{position: 'absolute', bottom: 0, right: 0}}
-                href="https://github.com/kasteckis/valorant-lineups"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <span>
-                    <Image src="/github.svg" alt="Github Logo" width={30} height={30} />
-                </span>
-            </a>
+                <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    p: 1,
+                    m: 1,
+                    borderRadius: 1,
+                }}>
+                    <AgentSelection />
+                </Box>
+
+                <a
+                    style={{position: 'absolute', bottom: 0, right: 0}}
+                    href="https://github.com/kasteckis/valorant-lineups"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <span>
+                        <Image src="/github.svg" alt="Github Logo" width={30} height={30} />
+                    </span>
+                </a>
+
+            </ThemeProvider>
         </>
     )
 }
